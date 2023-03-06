2e84b4503f97ec6ce8d67769904840bc9bf0c4de
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-03-06 10:40:49 -0300

    caos

--- components/Previsao.jsx
+++ components/Previsao.jsx
@@ -1,20 +1,14 @@
 import stylesButton from '../src/styles/Button.module.css'
 import stylesLayout from '../src/styles/Layout.module.css'
 
-export default function Menu(){
-    return(
 
-         <div className={stylesLayout.body}>
+export default function Previsao(){
+    return(
+        <div className={stylesLayout.body}>
             <div className={stylesLayout.topoVermelho}>Gêmeo Digital</div>        
             <div className={stylesLayout.topoAzul}>Planta de Manufatura Avançada</div>
             <div className={stylesLayout.ladoAzul}></div>
-
-            <div className={stylesButton.wrapper}>
-                <button className={stylesButton.button}>Wiki</button>
-                <button className={stylesButton.button}>Previsão</button>
-            </div>
                 
          </div>
-
     )
 }
