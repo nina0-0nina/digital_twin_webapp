71418d4706b6d6bfbc6758706ad684a35b46ff35
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-03-15 09:18:07 -0300

    tela Previso

--- components/Previsao.jsx
+++ components/Previsao.jsx
@@ -1,14 +1,18 @@
-import stylesButton from '../src/styles/Button.module.css'
 import stylesLayout from '../src/styles/Layout.module.css'
 
-
-export default function Previsao(){
-    return(
+export default function Previsao() {
+    return (
         <div className={stylesLayout.body}>
-            <div className={stylesLayout.topoVermelho}>Gêmeo Digital</div>        
-            <div className={stylesLayout.topoAzul}>Planta de Manufatura Avançada</div>
-            <div className={stylesLayout.ladoAzul}></div>
-                
-         </div>
+            <div className={stylesLayout.topo}>
+                <div className={stylesLayout.topoVermelho}>Gêmeo Digital</div>
+                <div className={stylesLayout.topoAzul}>Planta de Manufatura Avançada</div>
+                <div className={stylesLayout.ladoAzul}></div>
+            </div>
+
+            <div className={stylesLayout.wrapper}>
+                <button className={stylesLayout.button}>Previsão</button>
+            </div>
+        </div>
+
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import Menu from "../../components/Menu";
+import Previsao from "../../components/Previsao";
 
 export default function Home() {
     return (
         <div>
-            <Menu></Menu>
+            {/* <Menu></Menu> */}
+            <Previsao></Previsao>
         </div>
     )
 }
